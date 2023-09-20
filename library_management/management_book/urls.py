51e1726dcdc6558e99d288f2600efc8a8c9939aa
Junayed-shanto <junayedshanto54@gmail.com>
2023-09-20 22:06:40 +0600

    some edit in html

--- library_management/management_book/urls.py
+++ library_management/management_book/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('catalog/', views.book_catalog, name='book_catalog'),
     path('search/', views.book_search, name='book_search'),
     path('reservation/<str:book_id>/', views.book_reservation, name='book_reservation'),
-    # path('wishlist/add/<int:book_id>/', views.add_to_wishlist, name='add_to_wishlist')
 ]
